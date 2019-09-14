19261d65b8384858e7f0b1a163f74ec5b8a9c175
NamJinWoo <cjsencks@naver.com>
2019-09-14 16:13:39 -0700

    런타임에러

--- Baekjoon_15954.py
+++ Baekjoon_15954.py
@@ -2,11 +2,13 @@ import math
 import numpy as np
 N, K = input().split()
 array = [int(i) for i in input().split()]
+print(array)
 N = int(N)
 K = int(K)
 result = []
 for k in range(K, N+1):
     for i in range(0, N - k + 1):
+        print(array[i:i+k])
         average = sum(array[i:i+k]) / k
         # 분산 = 제곱의 평균 - 평균 * 평균
         a = np.array(array[i:i+k])
