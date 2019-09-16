0142eb90a6adfbfc7053be12732cadbb00ad6521
NamJinWoo <cjsencks@naver.com>
2019-09-15 19:03:20 -0700

    런탕임에러...

--- Baekjoon_15954.py
+++ Baekjoon_15954.py
@@ -1,22 +1,23 @@
 import math
-import numpy as np
-N, K = input().split()
+
+def funcMap(n):
+    return n * n
+
+
+N, K = map(int, input().split())
 array = [int(i) for i in input().split()]
-print(array)
-N = int(N)
-K = int(K)
 result = []
 for k in range(K, N+1):
     for i in range(0, N - k + 1):
-        print(array[i:i+k])
         average = sum(array[i:i+k]) / k
         # 분산 = 제곱의 평균 - 평균 * 평균
-        a = np.array(array[i:i+k])
-        a = a**2
-        test = sum(a) / k
+        m = list(map(funcMap, array[i:i+k]))
+
+        test = sum(m) / k
 
         new_average = test - average ** 2
+        if new_average >= 0:
+            result.append(math.sqrt(new_average))
 
-        result.append(math.sqrt(new_average))
 
 print("{0:.11f}".format(min(result)))
