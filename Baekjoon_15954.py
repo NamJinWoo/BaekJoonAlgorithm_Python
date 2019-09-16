0bd7d5ccd6b0782ea7a37df9bdc68b8ab296d29f
NamJinWoo <cjsencks@naver.com>
2019-09-15 21:46:22 -0700

    시간초과.....

--- Baekjoon_15954.py
+++ Baekjoon_15954.py
@@ -1,23 +1,30 @@
 import math
 
-def funcMap(n):
-    return n * n
-
-
 N, K = map(int, input().split())
 array = [int(i) for i in input().split()]
 result = []
+
+for k in range(K, N+1):
+    sliced_array = array
+    sum_array = []
+    #누적 합 배열
+    sum_array.append(array[0])
+    for i in range(1, len(array)):
+        sum_array.append(sum_array[i - 1] + array[i])
+
+    sq_sum_array = []
+    #누적 합 배열
+    sq_sum_array.append(array[0]**2)
+    for i in range(1,len(array)):
+        sq_sum_array.append(sq_sum_array[i-1]+array[i]**2)
+
 for k in range(K, N+1):
     for i in range(0, N - k + 1):
-        average = sum(array[i:i+k]) / k
         # 분산 = 제곱의 평균 - 평균 * 평균
-        m = list(map(funcMap, array[i:i+k]))
+        var = sq_sum_array[i+k-1] / k - sum_array[i+k-1] / k
 
-        test = sum(m) / k
-
-        new_average = test - average ** 2
-        if new_average >= 0:
-            result.append(math.sqrt(new_average))
+        if var >= 0:
+            result.append(math.sqrt(var))
 
 
 print("{0:.11f}".format(min(result)))
